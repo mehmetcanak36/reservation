b59f51b415fea35d34439c9b2006d1f9a01c0598
mehmet <mehmet.canak.36@gmail.com>
2020-12-28 13:54:30 +0300

    oda düzenleme sayfası eklendi

--- application/views/room/edit/main_content.php
+++ application/views/room/edit/main_content.php
@@ -11,6 +11,89 @@
                             <label for="exampleInputEmail1">Kategori Adı</label>
                             <input type="text" class="form-control" name="title" placeholder="Kategori adını giriniz.." value="<?php echo $row->title; ?>">                        </div>
                     </div>
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Oda Numarası</label>
+                            <input type="text" class="form-control" name="room_code"value="<?php echo $row->room_code; ?>">
+                        </div>
+                    </div>
+
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Başlık</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="box-body col-md-12">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Açıklama</label>
+                                <textarea id="detail" name="detail" rows="10" cols="80"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Oda Boyutu</label>
+                            <input type="text" class="form-control" name="size">
+                        </div>
+                    </div>
+
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Fiyatı(Gecelik)</label>
+                            <input type="text" class="form-control" name="default_price">
+                        </div>
+                    </div>
+
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label>Kategorisi</label>
+                            <select class="form-control" name="room_type_id">
+                                <?php foreach (get_room_category(array("isActive" => 1)) as $category){ ?>
+                                    <option value="<?php echo $category->id; ?>"><?php echo $category->title; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label>Kapasite</label>
+                            <select class="form-control" name="room_capacity">
+
+                                <?php for($i=1;$i<=10;$i++) { ?>
+                                    <option value="<?php echo $i; ?>"><?php echo "$i Kişilik";?></option>
+                                <?php } ?>
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label>Özellikler</label>
+                            <select name="room_properties[]" class="form-control select2" multiple="multiple" data-placeholder="Özellik Seçiniz" style="width: 100%;">
+                                <?php foreach (get_room_properties(array("isActive" => 1)) as $property){ ?>
+                                    <option value="<?php echo $property->id; ?>"><?php echo $property->title; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="box-body col-md-6">
+                        <div class="form-group">
+                            <label>extra servisler</label>
+                            <select name="room_extra_services[]" class="form-control select2" multiple="multiple" data-placeholder="Özellik Seçiniz" style="width: 100%;">
+                                <?php foreach (get_room_extra_services(array("isActive" => 1)) as $property){ ?>
+                                    <option value="<?php echo $property->id; ?>"><?php echo $property->title; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
                     <!-- /.box-body -->
                     <div class="clearfix"></div>
                     <div class="box-footer">
--- application/views/room/edit/page_style.php
+++ application/views/room/edit/page_style.php
@@ -1,1 +1,4 @@
 <link rel="stylesheet" href="<?php echo base_url("assets");?>/dist/css/third_party/bootstrap-toggle.min.css"/>
+
+
+<link rel="stylesheet" href="<?php echo base_url("assets");?>/plugins/select2/select2.min.css">
