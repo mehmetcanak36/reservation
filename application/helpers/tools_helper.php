628a18cc9f05e6fcfac9a5ed4ccca69c7b6923b8
mehmet <mehmet.canak.36@gmail.com>
2020-12-28 13:16:52 +0300

    helper eklendi

---
+++ application/helpers/tools_helper.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+function get_room_category(){
+    
+    $t = &get_instance();
+
+    $t->load->model("roomcategory_model");
+
+    $getRoom = $t->get_all();
+    return $getRoom;
+  
+
+}
+function get_room_properties(){
+    $t = &get_instance();
+
+    $t->load->model("roomproperties_model");
+
+    $getProperties = $t->get_all();
+    return $getProperties;
+
+}
+function get_room_extra_services($where = array())
+{
+
+    $t = &get_instance();
+    $t->load->model("roomextraservices_model");
+    return $t->roomextraservices_model->get_all($where);
+}
+
+
+?>
--- application/views/room/add/main_content.php
+++ application/views/room/add/main_content.php
@@ -2,9 +2,9 @@
 <section class="content">
     <div class="row">
         <div class="col-md-12">
-            <!-- general form elements -->
+            
             <div class="box box-primary">
-                <!-- form start -->
+                
                 <form role="form" method="post" action="<?php echo base_url("room/add");?>">
 
                     <div class="box-body col-md-6">
@@ -83,14 +83,14 @@
 
                 
 
-                    <!-- /.box-body -->
+                    
                     <div class="clearfix"></div>
                     <div class="box-footer">
                         <button type="submit" class="btn btn-primary">Ekle</button>
                     </div>
                 </form>
             </div>
-            <!-- /.box -->
+            
         </div>
 </section>
 <!-- /.content -->
